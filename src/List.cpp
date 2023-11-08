bf59bb8d80bf1fbeb6654ba9252918eb945d92c9
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-11-08 13:40:54 +0200

    The Assignment Operator for the List Class

--- src/List.cpp
+++ src/List.cpp
@@ -1,6 +1,14 @@
 #include "List.h"
 
 template <class T>
-List<T>& List::operator=(const List<T>& other) {
+List<T>& List<T>::operator=(const List<T>& other) {
+    if (this != &other) {
+        this->clear();
+        // Copying elements from the 'other' list to 'this' list
+        for (int i = 0; i < other.length(); i++) {
+            this->append(other.get(i));
+        }
+    }
 
+    return *this;
 }
