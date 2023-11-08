10061d75bc6690e50fa793445093d31402c1658a
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-11-09 00:37:50 +0200

    Queue Complete Implementation for the Project

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -2,40 +2,76 @@
 
 template <class T>
 Queue<T>::Queue() {
-    // Constructor implementation goes here
+    this->head = NULL;
+    this->tail = NULL;
 }
 
 template <class T>
 Queue<T>::Queue(const Queue<T>& other) {
-    // Copy constructor implementation goes here
+    this->head = this->tail = NULL;
+    if (other.isEmpty() == false) {
+        Node<T>* insertNode = other.head;
+        while (insertNode != NULL) {
+            this->enqueue(insertNode->data);
+            insertNode = insertNode->next;
+        }
+    }
 }
 
 template <class T>
 Queue<T>* Queue<T>::clone() const {
-    // clone implementation goes here
+    return new Queue(*this);
 }
 
 template <class T>
 Queue<T>::~Queue() {
-    // Destructor implementation goes here
+    if (this->isEmpty() == false) {
+        while (this->head != NULL) {
+            Node<T>* tempDelete = this->head;
+            this->head = this->head->next;
+            delete tempDelete;
+        }
+        this->tail = NULL;
+    }
 }
 
 template <class T>
 void Queue<T>::enqueue(T data) {
-    // enqueue implementation goes here
+    Node<T>* insertNode = new Node<T>(data);
+    if (this->tail == NULL && this->head == NULL) {
+        this->head = this->tail = insertNode;
+    } else {
+        this->tail->next = insertNode;
+        this->tail = insertNode;
+    }
 }
 
 template <class T>
 T Queue<T>::dequeue() {
-    // dequeue implementation goes here
+    if (this->isEmpty() == false) {
+        Node<T>* removedNode = this->head;
+        T removedData = removedNode->data;
+        this->head = this->head->next;
+        if (this->head == NULL) {
+            this->tail = NULL;
+        }
+        delete removedNode;
+        return removedData;
+    }
+
+    return T();
 }
 
 template <class T>
 const T Queue<T>::peek() const {
-    // peek implementation goes here
+    if (this->isEmpty() == false) {
+        return this->head->data;
+    } else {
+        return T();
+    }
 }
 
 template <class T>
 bool Queue<T>::isEmpty() const {
-    // isEmpty implementation goes here
+    return (this->head == NULL && this->tail == NULL);
 }
